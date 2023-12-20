1fa3b3914a5b9f20addff04e1561965fa93eeec9
tower1229 <tower1229@foxmail.com>
2023-12-20 09:51:17 +0800

    fix: balance tip

--- src/pages/game/_components/Header.tsx
+++ src/pages/game/_components/Header.tsx
@@ -132,7 +132,7 @@ const Header = forwardRef((_props, ref) => {
         <w3m-button />
       </header>
 
-      {isConnected && selectedNetworkId != String(Chain.id) && (
+      {isConnected && String(selectedNetworkId) !== String(Chain.id) && (
         <div className="wrap">
           <div role="alert" className="alert ">
             <svg
@@ -180,34 +180,36 @@ const Header = forwardRef((_props, ref) => {
               <span className="text-sm">{error.message}</span>
             </div>
           )}
-          {balanceData?.formatted && Number(balanceData.formatted) <= 0 && (
-            <div role="alert" className="alert ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="h-6 stroke-current w-6 shrink-0"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
-              </svg>
-              <span className="text-sm">
-                Your balance({balanceData.formatted}
-                {balanceData?.symbol}) is not enough!
-              </span>
-              <div>
-                <button className="btn btn-success btn-sm" disabled={isLoading}>
-                  {isLoading ? "requesting" : "request ETH"}
-                </button>
-              </div>
-            </div>
-          )}
         </div>
       )}
+      {String(selectedNetworkId) === String(Chain.id) &&
+        balanceData?.value &&
+        Number(balanceData.value) <= 100 && (
+          <div role="alert" className="alert ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="h-6 stroke-current w-6 shrink-0"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+            <span className="text-sm">
+              Your balance({balanceData.formatted}
+              {balanceData?.symbol}) is not enough!
+            </span>
+            <div>
+              <button className="btn btn-success btn-sm" disabled={isLoading}>
+                {isLoading ? "requesting" : "request ETH"}
+              </button>
+            </div>
+          </div>
+        )}
     </>
   );
 });
