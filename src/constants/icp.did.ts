30ce0bd044c5d70545d367faf6917b49c2da0769
tower1229 <tower1229@foxmail.com>
2024-01-25 18:56:29 +0800

    fix: lint

--- src/constants/icp.did.ts
+++ src/constants/icp.did.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 export const idlFactory = ({ IDL }) => {
   return IDL.Service({
     greet: IDL.Func([IDL.Text], [IDL.Text], []),
--- src/hooks/ctx/ToastRoot.tsx
+++ src/hooks/ctx/ToastRoot.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import { injectStyle } from "react-toastify/dist/inject-style";
 
-// eslint-disable-next-line react-refresh/only-export-components
+ 
 export const ToastRoot = function () {
   useEffect(() => {
     injectStyle();
